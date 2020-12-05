c30c0613586cf7f36e428887f9fab538dcf8f036
BenyaZhosh <benyazhosh@gmail.com>
2020-12-05 21:01:15 +0300

    Добавлены управление персонажем, реализация механик смерти, кнопок и прохождения уровня

---
+++ ReflectionsProject/Assets/Scripts/Map stuff/Finish/LevelFinish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelFinish : MonoBehaviour
+{
+    [SerializeField] private UnityEvent LevelEndsEvent;
+
+    private int _reachedPlayers = 0; // количество уроков на финише
+
+    private void OnPlayerReachedFinishEvent(bool reached)
+    {
+        if (reached)
+            _reachedPlayers++;
+        else
+            _reachedPlayers--;
+
+        if (_reachedPlayers >= 2)
+            LevelEndsEvent?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        LevelFinishDetector.PlayerReachedFinishEvent += OnPlayerReachedFinishEvent;
+    }
+    private void OnDisable()
+    {
+        LevelFinishDetector.PlayerReachedFinishEvent -= OnPlayerReachedFinishEvent;
+    }
+}
---
+++ ReflectionsProject/Assets/Scripts/Player/PlayerAnimationsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerAnimationsController : MonoBehaviour
+{
+    [SerializeField] private Animator _animatorController;
+    [Header("Animations names")]
+    [SerializeField] private string _idle;
+    [SerializeField] private string _run;
+    [SerializeField] private string _movingUp;
+    [SerializeField] private string _movingDown;
+
+    private PlayerMovement _player;
+
+    private void PlayAnimation()
+    {
+        if (!_player.IsGrounded)
+        {
+            if (_player.IsFalling)
+                _animatorController.Play($"{_movingDown}");
+            else
+                _animatorController.Play($"{_movingUp}");
+        }
+        else
+        {
+            if (_player.IsMoving)
+                _animatorController.Play($"{_run}");
+            else
+                _animatorController.Play($"{_idle}");
+        }
+    }
+
+    private void Awake()
+    {
+        _player = GetComponent<PlayerMovement>();
+    }
+    private void Update()
+    {
+        if (_animatorController)
+            PlayAnimation();
+    }
+}
